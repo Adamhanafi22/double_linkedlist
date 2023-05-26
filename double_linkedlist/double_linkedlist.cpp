b97791a4b276b98de19e5180b20474fc8367ddfa
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-05-26 19:29:18 +0700

    methoud traverse

--- double_linkedlist/double_linkedlist.cpp
+++ double_linkedlist/double_linkedlist.cpp
@@ -101,3 +101,15 @@ bool DoubleLinkedlist::deletenode(int rollNo) {
 bool DoubleLinkedlist::listEmpty() {
 	return (START == NULL);
 }
+void DoubleLinkedlist::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order or roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}	
+}
